8b3b8ec2addeb67b2c73b87ac95135914e2fdd7b
Syed Ali Rehan <104094853+mostlyali07@users.noreply.github.com>
2022-11-21 00:41:29 +0500

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import BreadCrumb from "./components/breadCrumb";
 import Banner from "./components/Banner";
 import Card_One from "./components/Card_One";
+import img from "../src/img_2.jpeg";
 
 function App() {
   return (
@@ -10,8 +11,20 @@ function App() {
       <Header />
       <BreadCrumb />
       <Banner />
-        <h4>Fresh recommendations</h4>
-      <Card_One title="oppo f19 lush condiction" price="RS 39,500" address="DHA Phase 3, Lahore" time="2 hours"/>
+      <div className="container">
+      <h4>Fresh recommendations</h4>
+        <div className="row">
+          <div className="col-md-3 Pro_one">
+            <img src={img} />
+            <Card_One
+              title="oppo f19 lush condiction"
+              price="RS 39,500"
+              address="DHA Phase 3, Lahore"
+              time="2 hours"
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/Card_One.js
+++ src/components/Card_One.js
@@ -1,18 +1,12 @@
 import React from "react";
-import img from "../src/img_2.jpeg";
 
 export default function (props) {
   return (
     <>
-      <div className="col-md-3">
-        <div className="product_img">
-          <img src={img}/>
-        </div>
-        <div className="text">
-          <h6>{props.title}</h6>
-          <h5>{props.price}</h5>
-          <span>{props.address}</span>•<span>{props.time}</span>
-        </div>
+      <div className="text">
+        <span>{props.title}</span><i class="fa-regular fa-heart"></i>
+        <h5>{props.price}</h5>
+        <span>{props.address}</span>•<span>{props.time}</span>
       </div>
     </>
   );
